8c96d6d1ec07fe1b5e18e134beab34a71946e5a3
Raz Lifshitz <razrezl@gmail.com>
2020-04-19 02:36:46 +0300

    - added on resize hook - press grid view - in progress

--- src/pages/press.js
+++ src/pages/press.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import styled from 'styled-components'
 // style
 import style from '../styles/press.module.scss'
@@ -7,18 +7,21 @@ import Layout from '../components/Layout'
 import NoStretchImage from '../components/NoStretchImage'
 //constants
 import { PRESS } from '../constants/one-of.constants'
+import { useResize } from '../hooks'
 // import portfolioSrc from '../assets/logos/portfolio.svg'
 
 const PressPage = ({ data }) => {
+    // constants
+    const ONE_COLUMN_WIDTH = '511px'
     // images
-    const designZoom = data.designZoom.childImageSharp.fluid
-    const coolHunting = data.coolHunting.childImageSharp.fluid
-    const PD = data.PD.childImageSharp.fluid
-    const DM = data.DM.childImageSharp.fluid
-    const culaizuv = data.culaizuv.childImageSharp.fluid
-    const portfolioSrc = data.portfolio.childImageSharp.fluid
-
-    const columnA = [
+    const designZoom = data.designZoom.childImageSharp.fixed
+    const coolHunting = data.coolHunting.childImageSharp.fixed
+    const PD = data.PD.childImageSharp.fixed
+    const DM = data.DM.childImageSharp.fixed
+    const culaizuv = data.culaizuv.childImageSharp.fixed
+    const portfolioSrc = data.portfolio.childImageSharp.fixed
+    // press list
+    const pressList = [
         {
             magazine: 'Design Milk',
             title:
@@ -29,30 +32,12 @@ const PressPage = ({ data }) => {
             logo: DM,
         },
         {
-            magazine: 'Design Zoom',
-            title: 'עבודת היד של המכונה',
-            author: 'Sigal Namir',
-            url:
-                'https://dezignzoom.co.il/%d7%a2%d7%91%d7%95%d7%93%d7%aa-%d7%94%d7%99%d7%93-%d7%a9%d7%9c-%d7%94%d7%9e%d7%9b%d7%95%d7%a0%d7%94-%d7%91%d7%95%d7%92%d7%a8%d7%99%d7%9d-2017/',
-            logo: designZoom,
-        },
-    ]
-    const columnB = [
-        {
             magazine: 'Cool Hunting',
             title: 'ONE OF A KIND MACHINE-MADE CERAMICS',
             url:
                 'https://coolhunting.com/tech/one-of-a-kind-machine-made-ceramics/',
             logo: coolHunting,
         },
-        {
-            magazine: 'culaizuv',
-            url:
-                'http://design.hit.ac.il/blog/%d7%94%d7%90%d7%93%d7%9d-%d7%94%d7%9e%d7%9b%d7%95%d7%a0%d7%94-%d7%95%d7%94%d7%9e%d7%93%d7%99%d7%94-%d7%94%d7%93%d7%99%d7%92%d7%99%d7%98%d7%9c%d7%99%d7%aa/',
-            logo: culaizuv,
-        },
-    ]
-    const columnC = [
         {
             magazine: 'Puro Diseno',
             title: 'Cerámica modelada a máquina',
@@ -62,45 +47,80 @@ const PressPage = ({ data }) => {
             logo: PD,
         },
         {
+            magazine: 'Design Zoom',
+            title: 'עבודת היד של המכונה',
+            author: 'Sigal Namir',
+            url:
+                'https://dezignzoom.co.il/%d7%a2%d7%91%d7%95%d7%93%d7%aa-%d7%94%d7%99%d7%93-%d7%a9%d7%9c-%d7%94%d7%9e%d7%9b%d7%95%d7%a0%d7%94-%d7%91%d7%95%d7%92%d7%a8%d7%99%d7%9d-2017/',
+            logo: designZoom,
+        },
+        {
+            magazine: 'culaizuv',
+            url:
+                'http://design.hit.ac.il/blog/%d7%94%d7%90%d7%93%d7%9d-%d7%94%d7%9e%d7%9b%d7%95%d7%a0%d7%94-%d7%95%d7%94%d7%9e%d7%93%d7%99%d7%94-%d7%94%d7%93%d7%99%d7%92%d7%99%d7%98%d7%9c%d7%99%d7%aa/',
+            logo: culaizuv,
+        },
+        {
             magazine: 'portfolio',
             url: 'https://www.prtfl.co.il/archives/126582',
             logo: portfolioSrc,
         },
     ]
-    const pressList = [columnA, columnB, columnC]
 
-    function getColumnJsx(pressList) {
-        return (
-            <PressColumn>
-                {pressList.map(pressItem => {
-                    return (
-                        <ImageCard
-                            key={pressItem.url}
-                            logo={pressItem.logo}
-                            url={pressItem.url}
-                        ></ImageCard>
-                    )
-                })}
-            </PressColumn>
+    // ref
+    const contentRef = useRef()
+    // state
+    const [isMobileView, setIsMobileView] = useState(null)
+    const [contentHeight, setContentHeight] = useState(null)
+
+    useResize(() => {
+        const result = window.matchMedia(`(max-width: ${ONE_COLUMN_WIDTH}`)
+        setIsMobileView(result.matches)
+
+        // calculating visible content height
+        // https://stackoverflow.com/questions/24768795/get-the-visible-height-of-a-div-with-jquery
+        const r = contentRef.current.getBoundingClientRect()
+        const elH = contentRef.current.offsetHeight
+        const t = r.top
+        const b = r.bottom
+        const H = window.innerHeight
+        const visibleContentHeight = Math.max(
+            0,
+            t > 0 ? Math.min(elH, H - t) : Math.min(b, H)
         )
-    }
+
+        setContentHeight(visibleContentHeight)
+    })
 
     return (
         <Layout activeItem={PRESS}>
             <Wrapper>
-                {pressList.map(pressColumn => {
-                    return getColumnJsx(pressColumn)
-                })}
+                <PressList
+                    ref={contentRef}
+                    contentHeight={contentHeight}
+                    isMobileView={isMobileView}
+                >
+                    {pressList.map(pressItem => {
+                        return (
+                            <ImageCard
+                                key={pressItem.url}
+                                logo={pressItem.logo}
+                                url={pressItem.url}
+                                isMobileView={isMobileView}
+                            ></ImageCard>
+                        )
+                    })}
+                </PressList>
             </Wrapper>
         </Layout>
     )
 }
 
-function ImageCard({ logo, url }) {
+function ImageCard({ logo, url, isMobileView }) {
     return (
-        <PressItem>
+        <PressItem isMobileView={isMobileView}>
             <PressLink href={url}>
-                <PressImage fluid={logo}></PressImage>
+                <PressImage fixed={logo}></PressImage>
             </PressLink>
         </PressItem>
     )
@@ -126,36 +146,40 @@ function ImageCard({ logo, url }) {
 
 export default PressPage
 
-const Wrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0 4px;
-`
+// Multi column W3School
 
-const PressColumn = styled.div`
-    flex: 33%;
-    max-width: 33%;
-    padding: 0 10px;
-`
+// const Wrapper = styled.div`
+//     display: flex;
+//     flex-wrap: wrap;
+//     padding: 0 4px;
+// `
 
-const PressItem = styled.div`
-    width: 190px;
-    height: 150px;
-    // margin: 0 auto;
-`
+// const PressColumn = styled.div`
+//     flex: 33%;
+//     max-width: 33%;
+//     padding: 0 10px;
+// `
 
-const PressLink = styled.a.attrs(props => ({
-    target: '_blank',
-    rel: 'noopener noreferrer',
-    href: props.href,
-}))``
+// const PressItem = styled.div`
+//     width: 190px;
+//     height: 150px;
+//     // margin: 0 auto;
+// `
 
-const PressImage = styled(NoStretchImage)`
-    // margin-top: 1.52rem !important;
-    // margin: auto 0 !important;
-    // vertical-align: middle !important;
-    // width: 100% !important;
-`
+// const PressLink = styled.a.attrs(props => ({
+//     target: '_blank',
+//     rel: 'noopener noreferrer',
+//     href: props.href,
+// }))``
+
+// const PressImage = styled(NoStretchImage)`
+//     // margin-top: 1.52rem !important;
+//     // margin: auto 0 !important;
+//     // vertical-align: middle !important;
+//     // width: 100% !important;
+// `
+
+//////////////////////////////
 
 // const Wrapper = styled.div`
 //     // width: 750px;
@@ -167,8 +191,8 @@ const PressImage = styled(NoStretchImage)`
 //     // list-style-type: none;
 
 //     display: grid;
-//     grid-template-columns: repeat(auto-fill, minmax(100px, 180px));
-//     grid-gap: 0 3rem;
+//     grid-template-columns: repeat(3, 1fr);
+//     grid-gap: 0 87px;
 //     grid-auto-rows: 150px;
 //     // justify-items: center;
 //     // height: 100%;
@@ -193,47 +217,79 @@ const PressImage = styled(NoStretchImage)`
 //     // grid-row-end: ${props => 'span ' + props.spans};
 // `
 
+const Wrapper = styled.div``
+
+const PressList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(238px, 1fr));
+    grid-template-rows: auto;
+    justify-items: center;
+
+    // web
+    margin-top: ${props => (props.isMobileView ? 'inherit' : '35px')};
+    grid-auto-rows: ${props => (props.isMobileView ? 'inherit' : '150px')};
+    // grid-gap: 0 87px;
+
+    // mobile
+    height: ${props =>
+        props.isMobileView ? props.contentHeight + 'px' : 'inherit'};
+    align-content: ${props =>
+        props.isMobileView ? 'space-evenly' : 'inherit'};
+`
+
+const PressItem = styled.div`
+    margin: ${props => (props.isMobileView ? 'inherit' : 'auto 0')};
+`
+
+const PressLink = styled.a.attrs(props => ({
+    target: '_blank',
+    rel: 'noopener noreferrer',
+    href: props.href,
+}))``
+
+const PressImage = styled(NoStretchImage)``
+
 export const query = graphql`
     query {
         designZoom: file(relativePath: { eq: "press/designZoom.png" }) {
             childImageSharp {
-                fluid(maxWidth: 190) {
-                    ...GatsbyImageSharpFluid_noBase64
+                fixed(width: 180) {
+                    ...GatsbyImageSharpFixed_noBase64
                 }
             }
         }
         coolHunting: file(relativePath: { eq: "press/coolHunting.png" }) {
             childImageSharp {
-                fluid(maxWidth: 190) {
-                    ...GatsbyImageSharpFluid_noBase64
+                fixed(width: 180) {
+                    ...GatsbyImageSharpFixed_noBase64
                 }
             }
         }
         PD: file(relativePath: { eq: "press/PD.png" }) {
             childImageSharp {
-                fluid(maxWidth: 190) {
-                    ...GatsbyImageSharpFluid_noBase64
+                fixed(width: 180) {
+                    ...GatsbyImageSharpFixed_noBase64
                 }
             }
         }
         DM: file(relativePath: { eq: "press/DM.png" }) {
             childImageSharp {
-                fluid(maxWidth: 190) {
-                    ...GatsbyImageSharpFluid_noBase64
+                fixed(width: 180) {
+                    ...GatsbyImageSharpFixed_noBase64
                 }
             }
         }
         culaizuv: file(relativePath: { eq: "press/culaizuv.png" }) {
             childImageSharp {
-                fluid(maxWidth: 190) {
-                    ...GatsbyImageSharpFluid_noBase64
+                fixed(width: 180) {
+                    ...GatsbyImageSharpFixed_noBase64
                 }
             }
         }
         portfolio: file(relativePath: { eq: "press/portfolio.png" }) {
             childImageSharp {
-                fluid(maxWidth: 190) {
-                    ...GatsbyImageSharpFluid_noBase64
+                fixed(width: 180) {
+                    ...GatsbyImageSharpFixed_noBase64
                 }
             }
         }
