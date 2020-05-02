f37815ae44164a644fe9b9ac1e010019530a7149
Raz Lifshitz <razrezl@gmail.com>
2020-05-03 01:50:18 +0300

    - gatsby-plugin-manifest: standalone --> browser - home: fixing footer not shrinking to top

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 // style
 import '../styles/index.scss'
 // components
 import layoutStyle from '../styles/layout.module.scss'
+import { useResize } from '../hooks'
 import Header from './Header'
 import Footer from './Footer'
 import SEO from './SEO'
 
 const Layout = ({ activeItem, children }) => {
+    const [contentHeight, setContentHeight] = useState(null)
+
+    useResize(() => {
+        setContentHeight(window.innerHeight)
+    }, [])
+
     return (
         <>
             <SEO></SEO>
-            <div className={layoutStyle.container}>
+            <Container
+                className={layoutStyle.container}
+                contentHeight={contentHeight}
+            >
                 <div className={layoutStyle.content}>
                     <Header activeItem={activeItem} />
                     {children}
                 </div>
 
                 <Footer />
-            </div>
+            </Container>
         </>
     )
 }
 
 export default Layout
+
+const Container = styled.div`
+    min-height: ${params =>
+        params.contentHeight ? params.contentHeight + 'px' : '100vh'};
+`
