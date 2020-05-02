660861358f63dd2f4d231887efdc4545eece5293
Raz Lifshitz <razrezl@gmail.com>
2020-05-02 03:17:38 +0300

    contact-success, 404 small fixes + styled-components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import styled from 'styled-components'
 // style
-import style from '../styles/contact.module.scss'
-
+import baseStyle from '../styles/base.module.scss'
 // components
 import Layout from '../components/Layout'
 import { usePlatesImages } from '../hooks/graphql/usePlatesImages'
 import RotatingPlateDialog from '../components/RotatingPlateDialog'
-
+import { GreyLink } from '../styled-components'
 // constants
 import { getRandIndex } from '../constants/one-of.constants'
 
@@ -23,17 +21,29 @@ const NotFoundPage = () => {
                 activeIndex={getRandIndex(images.length)}
                 isConstantImage={true}
             >
-                <div className={style.successMessage}>
-                    <h3 style={{ marginBottom: '2rem' }}>
+                <Wrapper>
+                    <Message>
                         We can't seem to find the page you're looking for.
-                    </h3>
-                    <Link className={style.button} to="/">
-                        ← Back Home
-                    </Link>
-                </div>
+                    </Message>
+                    <GreyLink to="/">← Back Home</GreyLink>
+                </Wrapper>
             </RotatingPlateDialog>
         </Layout>
     )
 }
 
 export default NotFoundPage
+
+const Wrapper = styled.div`
+    margin-top: 4.5rem;
+    animation: fade-in 1s;
+
+    @media (max-width: ${baseStyle.contactMobileDisplay}) {
+        margin-top: 0;
+        text-align: center;
+    }
+`
+
+const Message = styled.h3`
+    margin-bottom: 4rem;
+`
--- src/pages/contact-success.js
+++ src/pages/contact-success.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import styled from 'styled-components'
 // style
-import style from '../styles/contact.module.scss'
-
+import baseStyle from '../styles/base.module.scss'
 // componentes
 import Layout from '../components/Layout'
 import { usePlatesImages } from '../hooks/graphql/usePlatesImages'
 import RotatingPlateDialog from '../components/RotatingPlateDialog'
-
+import { GreyLink } from '../styled-components'
 // constants
 import { getRandIndex } from '../constants/one-of.constants'
 
@@ -23,18 +21,30 @@ const ContactSuccessPage = () => {
                 activeIndex={getRandIndex(images.length)}
                 isConstantImage={true}
             >
-                <div className={style.successMessage}>
-                    <h3 style={{ marginBottom: '7rem' }}>
+                <Wrapper>
+                    <Message>
                         I have received your message!<br></br>Will get back to
                         you shortly :)
-                    </h3>
-                    <Link className={style.button} to="/">
-                        ← Back Home
-                    </Link>
-                </div>
+                    </Message>
+                    <GreyLink to="/">← Back Home</GreyLink>
+                </Wrapper>
             </RotatingPlateDialog>
         </Layout>
     )
 }
 
 export default ContactSuccessPage
+
+const Wrapper = styled.div`
+    margin-top: 4.5rem;
+    animation: fade-in 1s;
+
+    @media (max-width: ${baseStyle.contactMobileDisplay}) {
+        margin-top: 0;
+        text-align: center;
+    }
+`
+
+const Message = styled.h3`
+    margin-bottom: 4rem;
+`
