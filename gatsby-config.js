88c3305c53cca333722f243496390f2c70a465dc
Raz Lifshitz <razrezl@gmail.com>
2020-05-02 18:33:34 +0300

    new packages: - gatsby-plugin-manifest - gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,28 @@ module.exports = {
             },
         },
         'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Ofri Lifshitz Design`,
+                short_name: `Ofri Lifshitz`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#ffffff`,
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: `standalone`,
+                icon: `src/images/icon.png`, // This path is relative to the root of the site.
+                cache_busting_mode: 'none',
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-offline',
+            options: {
+                workboxConfig: {
+                    globPatterns: ['**/*'],
+                },
+            },
+        },
     ],
 }
